5c90d9520e1e2090ba82ae048d4916ab3c29880e
PanPapag <pan_papageorgiou@hotmail.com>
2019-08-05 20:58:03 +0300

    Arguments parsing completed

---
+++ src/main.py
@@ -0,0 +1,43 @@
+import argparse
+
+def make_args_parser():
+    # create an ArgumentParser object
+    parser = argparse.ArgumentParser(description='Applied machine learning in automated text categorization')
+    # filling parser with information about program arguments
+    parser.add_argument('--datasets', default='datasets',
+                         help='Define the relative path of the dataset directory')
+    parser.add_argument('--outputs', default='datasets',
+                         help='Define the relative path of the output directory')
+    parser.add_argument('--duplicates', dest='threshold', type=float, default=None,
+                         help='Find similar documents within above a certain theta')
+    parser.add_argument('--preprocess', action='store_true', default=True,
+                        help='Articles preprocessing')
+    parser.add_argument('--wordcloud', action='store_true', default=False,
+                        help='Generate word clouds for each article category')
+    parser.add_argument('--classification', choices=['NB', 'RF', 'SVM', 'KNN'], default=None,
+                        help='''Runs default classifiers: Naive Bayes, Random Forest,
+                        Support Vector Machine and K-Nearest Neighbor''')
+    parser.add_argument('--kfold', action='store_true',
+                        help='Evaluate and report the performance of each method using 10-fold Cross Validation')
+    parser.add_argument('--cache', action='store_true',
+                        default=False, help='Use already preprocessed data')
+    # return an ArgumentParser object
+    return parser.parse_args()
+
+def print_args(args):
+    print('---------------------- Applied machine learning in automated text categorization ----------------------\n')
+    print("Running with the following configuration")
+    # get the __dict__ attribute of args using vars() function
+    args_map = vars(args)
+    for key in args_map:
+        print('\t', key, '-->', args_map[key])
+    # add one more empty line for better output
+    print()
+
+def main():
+    # parse and print arguments
+    args = make_args_parser()
+    print_args(args)
+
+if __name__ == '__main__':
+    main()
