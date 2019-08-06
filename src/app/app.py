6ed4b30fdb3e7d5c834a2f2e3c80147f86fd87fa
PanPapag <pan_papageorgiou@hotmail.com>
2019-08-06 13:53:10 +0300

    __init__ method in class App completed

--- src/app/app.py
+++ src/app/app.py
@@ -6,6 +6,7 @@ class App:
     def __init__(self, datasets, outputs, threshold=None, preprocess=False,
                  wordcloud=False, classification=None, features=None, kfold=False, cache=False):
 
+        # pass info from arguments
         self.datasets = datasets
         self.outputs = outputs
         self.threshold = threshold
@@ -16,5 +17,40 @@ class App:
         self.kfold = kfold
         self.cache = cache
 
+        # define csv train and test file names
+        if not self.cache:
+            self.csv_train_file = "/".join([datasets,'train_set.csv'])
+            self.csv_test_file = "/".join([datasets,'test_set.csv'])
+        else:
+            self.csv_train_file = "/".join([datasets,'processed_train_set.csv'])
+            self.csv_test_file = "/".join([datasets,'processed_test_set.csv'])
+
+        # read csv train and test files using pandas
+        self.train_df = pd.read_csv(self.csv_train_file, sep='\t')
+        self.test_df = pd.read_csv(self.csv_test_file, sep='\t') if not self.kfold else None
+
+        # define output directory names
+        self.wordcloud_out_dir = "/".join([outputs,'wordcloud_out_dir/']) if self.wordcloud else None
+        self.duplicates_out_dir = "/".join([outputs,'duplicates_out_dir/']) if self.threshold else None
+        self.classification_out_dir = "/".join([outputs,'classification_out_dir/']) if self.classification else None
+
+        # create output directories (if not already exist)
+        if not os.path.exists(self.outputs):
+            os.makedirs(self.outputs)
+
+        if self.wordcloud:
+            if not os.path.exists(self.wordcloud_out_dir):
+                os.makedirs(self.wordcloud_out_dir)
+
+        if self.threshold:
+            if not os.path.exists(self.duplicates_out_dir):
+                os.makedirs(self.duplicates_out_dir)
+
+        if self.classification:
+            if not os.path.exists(self.classification_out_dir):
+                os.makedirs(self.classification_out_dir)
+
+
     def run(self):
         print("App running")
+        print(self.train_df)
