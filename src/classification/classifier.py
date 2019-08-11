0a8b4108698c69b9c347a0693d68308e0cc5ed37
PanPapag <pan_papageorgiou@hotmail.com>
2019-08-11 16:43:38 +0300

    constructors created

--- src/classification/classifier.py
+++ src/classification/classifier.py
@@ -1,3 +1,7 @@
+import gensim
+import numpy as np
+
+from sklearn import preprocessing
 
 
 class Classifier:
@@ -6,4 +10,12 @@ class Classifier:
         # pass info from arguments
         self.path = path
         self.feature = feature
-        
+        # Encode labels with value the different articles' categories
+        self.le = preprocessing.LabelEncoder()
+        self.le.fit(train_df['Category'])
+        # define x_train and y_train
+        self.x_train = train_df['Content']
+        self.y_train = self.le.transform(train_df['Category'])
+        # define x_test
+        self.x_test = test_df['Content']
+        self.test_ids = test_df['Id']
